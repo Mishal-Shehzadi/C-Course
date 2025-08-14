9b9739b929f8be76170e613458d1ea2b4dded193
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-14 16:52:11 +0500

    Add files via upload

---
+++ 14-August-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  //it is used to get input output
+using namespace std; // helps to not write std:: again and again
+
+int main()
+{
+    cout << "My name is Mishal Shehzadi" << endl;
+    // endl finishes the line so that next thing will appear in the next line
+
+    cout << "My name is Mishal Shehzadi\n";
+    // it is also used to go to the next line (it is called next line)
+    //<< is called insertion operator
+
+    cout << 3 + 2;
+
+    return 0;
+}
