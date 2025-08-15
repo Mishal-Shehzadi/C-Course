4c06fa08110281dc2f0c666ddcfe50cac04394b9
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-15 14:59:50 +0500

    Add files via upload

---
+++ 15-August-f.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ // check if the number is vowel or consonant
+    char c;
+    cout << "Enter any character to check if it is vowel or consonanat: ";
+    cin >> c;
+
+    if (c == 'a')
+    {
+        cout << "Vowel";
+    }
+    else if (c == 'e')
+    {
+        cout << "Vowel";
+    }
+    else if (c == 'i')
+    {
+        cout << "Vowel";
+    }
+    else if (c == 'o')
+    {
+        cout << "Vowel";
+    }
+    else if (c == 'u')
+    {
+        cout << "Vowel";
+    }
+    else
+    {
+        cout << "Consonant";
+    }
+    return 0;
+}
