ed55fba23b53643a5e1e9eb1b5e3dc659a88208b
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-16 15:13:35 +0500

    Add files via upload

---
+++ 16-August-c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print 1 4 7 10 and so on
+
+    for (int i = 1; i <= 100; i = i + 3)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
