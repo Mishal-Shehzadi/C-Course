a13e06654de00df1c6677e622ca72a5b086ad769
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-18 14:37:40 +0500

    Add files via upload

---
+++ 18-August-i.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 5
+    // 5 4
+    // 5 4 3
+    // 5 4 3 2
+    // 5 4 3 2 1
+
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 5; col >= 5-(row-1); col--)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
