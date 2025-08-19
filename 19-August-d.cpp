18b435b49ebb772a6e6876c007a803feda2cdc39
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-19 15:29:52 +0500

    Add files via upload

---
+++ 19-August-d.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //         A
+    //       A B
+    //     A B C
+    //   A B C D
+    // A B C D E
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n - row; col++)
+        {
+            cout << "  ";
+        }
+        for (int col = 1; col <= row; col++)
+        {
+            char name = 'A' + col - 1;
+
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
