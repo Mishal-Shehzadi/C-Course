c4c9640fc5f382da021c23388b76f9d8f6f4b830
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-20 15:50:28 +0500

    Add files via upload

---
+++ 20-August-e.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // *             *
+    // * *         * *
+    // * * *     * * *
+    // * * * * * * * *
+    // * * *     * * *
+    // * *         * *
+    // *             *
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+        for (int col = 2 * n - 2 * row; col >= 1; col--)
+        {
+            cout << "  ";
+        }
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for (int row = n - 1; row >= 1; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+        for (int col = 1; col <= 2*n-2*row; col++)
+        {
+            cout << "  ";
+        }
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
