2fe6c8f8def29dad4d45f8f969b849e68d357f0b
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-22 16:05:42 +0500

    Add files via upload

---
+++ 22-August-h.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print A to Z using while loop
+
+    char i = 'A';
+    while (i <= 'Z')
+    {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
