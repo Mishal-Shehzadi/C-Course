5d4675bdd4695c51e8bf7b7fe6df6ca5bf05266f
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-08-25 18:06:02 +0500

    Add files via upload

---
+++ 25-August-c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // find trailing zero in a fact
+    // !8=40320 so we have only 1 zero in the end therefore answer will be 1
+    
+
+    return 0;
+}
