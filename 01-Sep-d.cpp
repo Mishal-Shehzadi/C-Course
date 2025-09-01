fd5f42d7db313368c829b08450d0ef9c4cce8e8b
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-01 19:31:07 +0500

    Add files via upload

---
+++ 01-Sep-d.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print z to a
+
+    for (char i = 'z'; i >= 'a'; i--)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
