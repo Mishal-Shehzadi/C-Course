15cc07208841d83f0c5797226c704172d391fe83
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-01 19:30:17 +0500

    Add files via upload

---
+++ 01-Sep-a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //print product of two numbers
+    
+    int num1;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    int num2;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "The product of " << num1 << " and " << num2 << " is " << num1 * num2;
+
+    return 0;
+}
