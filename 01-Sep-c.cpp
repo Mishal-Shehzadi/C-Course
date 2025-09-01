d61814aab27802acd41e32184e90bea2fcf69c5b
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-01 19:30:50 +0500

    Add files via upload

---
+++ 01-Sep-c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print number from 280  to 250
+
+    for (int i = 280; i >= 250; i--)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
