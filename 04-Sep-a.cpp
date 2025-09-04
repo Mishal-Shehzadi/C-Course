281425afbb50d6b362f86fc5f413461c05d46d91
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-04 21:01:28 +0500

    Add files via upload

---
+++ 04-Sep-a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // while loop
+
+    int i = 1;
+    while (i <= n)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+        i++;
+    }
+
+    return 0;
+}
