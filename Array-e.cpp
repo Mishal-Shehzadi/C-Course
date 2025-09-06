1999c6e72b79dff650281f23090038e540f8caee
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-06 18:05:40 +0500

    Add files via upload

---
+++ Array-e.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[] = {'a', 'b', 'c', 'd'};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << name[i] << " ";
+    }
+
+    return 0;
+}
