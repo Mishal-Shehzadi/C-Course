bea417dd0e7ae15577f040aa81fd5b7ed3d13352
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-07 23:46:12 +0500

    Add files via upload

---
+++ Strings-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string n;
+    cout << "Enter a line: ";
+    getline(cin, n);
+
+    cout << "You wrote: " << n;
+
+    return 0;
+}
