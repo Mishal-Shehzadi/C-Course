6c3e320f198458b38ab686ce45d111d9f240b685
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-07 23:46:32 +0500

    Add files via upload

---
+++ Strings-c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter name: ";
+    cin>>name;
+
+    name.push_back('P');
+
+    cout<<name;
+
+
+
+    return 0;
+}
