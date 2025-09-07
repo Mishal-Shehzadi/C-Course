7fda46901b6eb9d6678515111accc98eb06d2746
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-07 23:46:52 +0500

    Add files via upload

---
+++ Strings-d.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "Mishal is a \"good\" girl";
+    cout << s << endl;
+
+    string n = "\\";
+    cout << n;
+
+    return 0;
+}
