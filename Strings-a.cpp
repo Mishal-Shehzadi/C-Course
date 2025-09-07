fa78763434925f4ec714b505d64b7dae55e0e21d
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-07 23:45:49 +0500

    Add files via upload

---
+++ Strings-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[] = {'A', 'P', 'P', 'L', 'E'};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i];
+    }
+
+    return 0;
+}
