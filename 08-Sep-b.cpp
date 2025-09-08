28563016770584695fd080c7405451d1e229927b
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-08 20:46:08 +0500

    Add files via upload

---
+++ 08-Sep-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // rotate array by 1
+
+    int arr[] = {2, 7, 4, 11, 5, 8};
+    int last = 8;
+
+    int n = 6;
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = last;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
