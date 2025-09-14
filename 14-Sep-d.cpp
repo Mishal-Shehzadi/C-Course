b8ec0dec6fa93427955ba002f7d94dcb144a9d40
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-09-14 15:17:33 +0500

    Add files via upload

---
+++ 14-Sep-d.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    // rotate matrix by 90 degree u
+    // step 01: transpose matrix
+    // step 02: reverse row
+
+    int arr[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    int n = 4;
+
+    // transpose matrix
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+
+    cout << "Matrix after transposing: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // reverse rows
+
+    for (int i = 0; i < n; i++)
+    {
+        int start = 0;
+        int end = n - 1;
+        while (start < end)
+        {
+            swap(arr[i][start], arr[i][end]);
+            start++;
+            end--;
+        }
+    }
+
+    cout << "Matrix after rotation by 90: " << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
