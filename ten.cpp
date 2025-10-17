90985ca5496d213140c94a6a34fa72cdfe75ebb6
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-10-17 23:16:38 +0500

    Add files via upload

---
+++ ten.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    for (int row=1; row<=n; row++){
+        char name='a'+row-1;
+        for (int col=1; col<=n-row; col++){
+            cout<<"  ";
+        }
+        for (int col=1; col<=row; col++){
+            cout<<name<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
