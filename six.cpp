e5ca4a7899b826d5984896c2903827c98e0c41ff
Mishal Shehzadi <mishalshehzadi89@gmail.com>
2025-10-17 23:14:39 +0500

    Add files via upload

---
+++ six.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = row; col >= 1; col--)
+        {
+            char name = 'a' + col - 1;
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
